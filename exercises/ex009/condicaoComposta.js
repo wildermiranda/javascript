d5f1db08fa56c36e203c6dbf1f51b74097e2a1ac
Wilder Miranda <wildermiranda99@gmail.com>
2022-02-13 20:42:01 -0300

    Create a new exercise ex009

---
+++ exercises/ex009/condicaoComposta.js
@@ -0,0 +1,10 @@
+var country = 'Estados Unidos'
+
+console.log(`Vivendo em ${country}.`)
+
+if (country == 'Brasil') {
+    console.log('Você é brasileiro!')
+
+} else {
+    console.log('Você é um estrangeiro!')
+}
---
+++ exercises/ex009/condicaoSimples.js
@@ -0,0 +1,9 @@
+var speed = 60.5
+
+console.log('Dirija sempre usando cinto de segurança.')
+console.log(`A velocidade do seu carro é ${speed} Km/h.`)
+
+// Condição Simples
+if (speed > 60) {
+    console.log('Você ultrapassou a velocidade permitida!')
+}
