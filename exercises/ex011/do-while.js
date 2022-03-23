88c626461d85ea903603b0113f613aeec863059a
Wilder Miranda <wildermiranda99@gmail.com>
2022-03-23 14:51:48 -0300

    Create a new exercise

---
+++ exercises/ex011/do-while.js
@@ -0,0 +1,8 @@
+var contador = 1
+
+// Enquanto contador for menor que 6
+do {
+    console.log('How are you?')
+    contador++ // contador + 1
+
+} while (contador <= 6) 
---
+++ exercises/ex011/while.js
@@ -0,0 +1,16 @@
+/* 
+console.log('How are you?')
+console.log('How are you?')
+console.log('How are you?')
+console.log('How are you?')
+console.log('How are you?')
+console.log('How are you?') */
+
+
+var contador = 1
+
+// Enquanto contador for menor que 6
+while (contador <= 6) {
+    console.log('How are you?')
+    contador++ // contador + 1
+}
