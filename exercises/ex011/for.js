d124ebf1601126b40ec9ce20116618ccb6a711af
Wilder Miranda <wildermiranda99@gmail.com>
2022-04-02 21:12:05 -0300

    New for.js file

---
+++ exercises/ex011/for.js
@@ -0,0 +1,20 @@
+/* 
+// While
+
+var contador = 1
+
+while (contador <= 4) {
+    console.log('You are learning JavaScript! 🎉')
+    contador++
+} */
+
+
+// For 
+
+console.log('Vai começar! 🏁')
+
+for (var contador = 1; contador <= 4; contador++) {
+    console.log('You are learning JavaScript! 🎉')
+}
+
+console.log('Fim 🏁')
