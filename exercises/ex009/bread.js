6b6b5174b37a8b2b5f57646fca690d5059267edb
Wilder Miranda <wildermiranda99@gmail.com>
2022-12-15 16:33:04 -0300

    Added bread.js file

---
+++ exercises/ex009/bread.js
@@ -0,0 +1,9 @@
+const bread = 'Fresco'
+
+console.log(`O pão está ${bread}`)
+
+if (bread == 'Fresco') {
+    console.log('Compre o pão! 🥖')
+} else {
+    console.log('Passe em outra padaria! 🏃')
+}
