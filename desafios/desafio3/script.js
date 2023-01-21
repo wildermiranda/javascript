f297043f63d3de53d6514f62fe4d14fefcd913e8
Wilder Miranda <wildermiranda99@gmail.com>
2023-01-21 08:57:47 -0300

    Added desafio3 folder

--- desafios/desafio3/script.js
+++ desafios/desafio3/script.js
@@ -1,18 +1,25 @@
-var button = document.querySelector('button')
+const button = document.querySelector('#button')
 
-button.addEventListener('click', function() {
-    let inicio = document.querySelector('#inicio')
-    let fim = document.querySelector('#fim')
-    let intervalo = document.querySelector('#intervalo')
-    let resultado = document.querySelector('#resultado')
+button.addEventListener('click', () => {
+    let inicio = Number(document.querySelector('#inicio').value)
+    const fim = Number(document.querySelector('#fim').value)
+    const intervalo = Number(document.querySelector('#intervalo').value)
+    const output = document.querySelector('output')
 
-    //.trim() removendo whitespace de ambos os lados
-    let inicioValue = inicio.value.trim()
-    let fimValue = fim.value.trim()
-    let intervaloValue = intervalo.value.trim()
+    output.innerHTML = ''
 
-    if (inicioValue === '' || fimValue === '' || intervaloValue === '') {alert('Error!')}
-}); 
+    if (inicio == '' || fim == '' || intervalo == '') {
+        alert('Por favor, preencha todos os campos.')
+    
+    } else if (inicio <= 0 || fim <= 0 || intervalo <= 0) {
+        alert('Digite apenas números positivos')
+    
+    } else {
+        for (inicio; inicio <= fim; inicio += intervalo) {
+            let number = inicio
+            output.innerHTML += `&nbsp&nbsp 👉 ${number}`
+        }
 
-
-
+        output.innerHTML += `&nbsp&nbsp&nbsp&nbsp 🏁`
+    }
+})
