5fb14f2e827d3a11aa7ea6d978343a43176d683b
Wilder Miranda <wildermiranda99@gmail.com>
2023-01-30 22:41:54 -0300

    Desafio3 has been modified

--- desafios/desafio3/script.js
+++ desafios/desafio3/script.js
@@ -1,25 +1,46 @@
 const button = document.querySelector('#button')
 
 button.addEventListener('click', () => {
-    let inicio = Number(document.querySelector('#inicio').value)
-    const fim = Number(document.querySelector('#fim').value)
-    const intervalo = Number(document.querySelector('#intervalo').value)
+    let inicio = document.querySelector('#inicio').value
+    const fim = document.querySelector('#fim').value
+    const intervalo = document.querySelector('#intervalo').value
     const output = document.querySelector('output')
 
     output.innerHTML = ''
-
-    if (inicio == '' || fim == '' || intervalo == '') {
-        alert('Por favor, preencha todos os campos.')
     
-    } else if (inicio <= 0 || fim <= 0 || intervalo <= 0) {
-        alert('Digite apenas números positivos')
+    
+    if (inicio.length == 0 || fim.length == 0 || intervalo.length == 0) {
+        alert('Preencha os campos corretamente!')
     
     } else {
-        for (inicio; inicio <= fim; inicio += intervalo) {
-            let number = inicio
-            output.innerHTML += `&nbsp&nbsp 👉 ${number}`
-        }
+        let i = +inicio
+        let f = +fim
+        let int = +intervalo
 
-        output.innerHTML += `&nbsp&nbsp&nbsp&nbsp 🏁`
-    }
+        if (int == 0) {
+            alert('O intervalo precisa ser maior que zero!')
+            int++
+        } 
+        
+    
+        if (i < 0 || f < 0 || int < 0) {
+            alert('Digite apenas números positivos!')
+        
+        } else {
+            if (i < f) {
+                for (i; i <= f; i += int) {
+                    let number = i
+                    output.innerHTML += `&nbsp&nbsp 👉 ${number}`
+                }
+            
+            } else {
+                for (i; i >= f; i -= int) {
+                    let number = i
+                    output.innerHTML += `&nbsp&nbsp 👉 ${number}`
+                }
+            }
+
+            output.innerHTML += `&nbsp&nbsp&nbsp&nbsp 🏁`
+        }
+    } 
 })
